63b23af311a1baaea5863264648ba389ada6fbd6
Opilane TTHK <opilane@tthk.ee>
2022-03-08 08:31:56 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using KutseApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KutseApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Greeting = DateTime.Now.Hour < 12 ? "Tere hommikust" : $"Tere päevast";
+			ViewBag.Message = "Ootan sind oma peole! Palun tule kindlasti";
+			return View();
+		}
+		[HttpGet]
+		public ViewResult Ankeet()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ViewResult Ankeet(Guest guest)
+		{
+			if (ModelState.IsValid)
+			{
+				if(guest.Attend == true)
+				{
+					SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+					{
+						Port = 587,
+						Credentials = new NetworkCredential("programmeeriminetthk2@gmail.com", "2.kuursus tarpv20"),
+						EnableSsl = true,
+					};
+					MailMessage mailMessage = new MailMessage
+					{
+						From = new MailAddress("Cinema.Amogus@service.com"),
+						Subject = "Kutse peole",
+						Body = $"<h1>Ära unusta et sul on peole kutse!</h1>",
+						IsBodyHtml = true,
+					};
+
+					mailMessage.To.Add(new MailAddress(guest.Email));
+					smtpClient.Send(mailMessage);
+				}
+				return View("Thanks", guest);
+			}
+			else
+				return View();
+		}
+		public ActionResult About()
+		{
+			ViewBag.Message = "Haha.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Hehe.";
+
+			return View();
+		}
+	}
+}
---
+++ Models/Guest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KutseApp.Models
+{
+	public class Guest
+	{
+		public int Id;
+		[Required(ErrorMessage ="On vaja sisesta oma nime!")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "On vaja sisesta oma email!")]
+		[RegularExpression(@".+\@.+\..+", ErrorMessage = "See on vale email!")]
+		public string Email { get; set; }
+		[Required(ErrorMessage = "On vaja sisesta oma telefoninumber!")]
+		[RegularExpression(@"\+372.+", ErrorMessage = "See on vale telefon!")]
+		public string Phone { get; set; }
+		[Required(ErrorMessage = "Tee oma valik!")]
+		public bool? Attend { get; set; }
+	}
+}
